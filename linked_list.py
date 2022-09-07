961856d5e1ce72a731e8d1eb0201bac84089f2bc
Samir Ansari <developedbysam@gmail.com>
2022-09-07 12:00:51 -0500

    implemented insert method in linked list

--- linked_list.py
+++ linked_list.py
@@ -29,10 +29,37 @@ class LinkedList(object):
         self.head = new_node
         self.length += 1
 
+    def insert(self, pos, value):
+        # MAKE IT THE LAST ITEM IF INSERT POSITION IS GREATER THAN THE LIST LENGTH
+        if pos >= self.length:
+            return self.append(value)
+        # MAKE IT HEAD IF THE INSERT POSITION IS 0
+        if pos == 0:
+            return self.prepend(value)
+        if pos < 0:
+            raise IndexError('Incorrect insert position, list index out of bound!!')
+        new_node = Node(value)
+        leader = self.traverse_to_index(pos - 1)
+        next_holding_pointer = leader.next
+        leader.next = new_node
+        new_node.next = next_holding_pointer
+        self.length += 1
+
+    def traverse_to_index(self, index):
+        current_pos = 0
+        current_node = self.head
+        while current_pos != index:
+            current_node = current_node.next
+            current_pos += 1
+        return current_node
+
 
 my_linked_list = LinkedList(10)
 my_linked_list.append(5)
 my_linked_list.append(16)
 my_linked_list.prepend(1)
 my_linked_list.prepend(2)
+my_linked_list.insert(2, 75)
+my_linked_list.insert(200, 75)
+my_linked_list.insert(0, 750)
 print(my_linked_list)
